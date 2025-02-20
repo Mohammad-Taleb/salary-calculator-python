01426c107f7c20fc287f17e7d5b749ecf5cdf103
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-02-20 20:51:36 +0200

    Add new print lines <> to print out the menu

--- salary-program.py
+++ salary-program.py
@@ -23,3 +23,16 @@ elif month_name not in months:
     print("Enter a valid month name")
 else:
    while True:
+        print()
+        print("********************************************************************")
+        print("CHOOSE A NUMBER TO EXECUTE ONE OF THE CHOICES")
+        print("1- TO DEPOSIT TO YOUR SAVINGS")
+        print("2- TO PAY THE RENT")
+        print("3- TO PAY THE ELECTRICITY BILL")
+        print("4- TO CHECK YOUR TOTAL SAVINGS")
+        print("5- TO CHECK THE REMAINDER OF THE SALARY (WALLET)")
+        print("6- TO CHECK THE YEARLY ESTIMATION COST FOR THE RENT AND ELECTRICITY")
+        print("7- T0 CHECK YOUR TOTAL EXPENSES")
+        print("0- TO STOP THE PROGRAM")
+        print("*********************************************************************")
+        print()
