3713a9b52692f1d566239603540976e3fc2d0933
Mohammad-Taleb <akamohdtaleb@gmail.com>
2025-02-20 20:46:15 +0200

    Add user inputs <> asking for salary and month  name

--- salary-program.py
+++ salary-program.py
@@ -14,3 +14,5 @@ months = ['january', 'february', 'march', 'april', 'may',
           'june', 'july', 'august', 'september', 'october', 'november', 'december']
 
 
+salary = float(input("Enter your salary in $: "))
+month_name = input("Enter the name of the month: ").lower()
